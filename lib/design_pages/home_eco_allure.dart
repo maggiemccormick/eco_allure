fe755b9319b1b879a918bf432fda07584fe7a571
Maggie McCormick <magsemail2000@gmail.com>
2022-03-28 17:26:11 -0700

    💄🍱ui updates
    -added image and font assets 
    -restructed ui to work for ecoallure 
    -renamed files and classes
    -changed color of background

---
+++ lib/design_pages/home_eco_allure.dart
@@ -0,0 +1,311 @@
+import 'category_list_view.dart';
+import 'brand_info_screen.dart';
+import 'popular_brands_list_view.dart';
+import 'package:eco_allure/main.dart';
+import 'package:flutter/material.dart';
+import 'eco_allure_app_theme.dart';
+
+class EcoAllureHomeScreen extends StatefulWidget {
+  @override
+  _EcoAllureHomeScreenState createState() => _EcoAllureHomeScreenState();
+}
+
+class _EcoAllureHomeScreenState extends State<EcoAllureHomeScreen> {
+  CategoryType categoryType = CategoryType.skincare;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: EcoAllureAppTheme.notWhite,
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        body: Column(
+          children: <Widget>[
+            SizedBox(
+              height: MediaQuery.of(context).padding.top,
+            ),
+            getAppBarUI(),
+            Expanded(
+              child: SingleChildScrollView(
+                child: Container(
+                  height: MediaQuery.of(context).size.height,
+                  child: Column(
+                    children: <Widget>[
+                      getSearchBarUI(),
+                      getCategoryUI(),
+                      Flexible(
+                        child: getPopularCourseUI(),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget getCategoryUI() {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Padding(
+          padding: const EdgeInsets.only(top: 8.0, left: 18, right: 16),
+          child: Text(
+            'Product Type',
+            textAlign: TextAlign.left,
+            style: TextStyle(
+              fontWeight: FontWeight.w600,
+              fontSize: 22,
+              letterSpacing: 0.27,
+              color: EcoAllureAppTheme.darkerText,
+            ),
+          ),
+        ),
+        const SizedBox(
+          height: 16,
+        ),
+        Padding(
+          padding: const EdgeInsets.only(left: 16, right: 16),
+          child: Row(
+            children: <Widget>[
+              getButtonUI(CategoryType.skincare, categoryType == CategoryType.skincare),
+              const SizedBox(
+                width: 16,
+              ),
+              getButtonUI(
+                  CategoryType.makeup, categoryType == CategoryType.makeup),
+              const SizedBox(
+                width: 16,
+              ),
+              getButtonUI(
+                  CategoryType.health, categoryType == CategoryType.health),
+            ],
+          ),
+        ),
+        const SizedBox(
+          height: 16,
+        ),
+        CategoryListView(
+          callBack: () {
+            moveTo();
+          },
+        ),
+      ],
+    );
+  }
+
+  Widget getPopularCourseUI() {
+    return Padding(
+      padding: const EdgeInsets.only(top: 8.0, left: 18, right: 16),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Text(
+            'Popular Brands',
+            textAlign: TextAlign.left,
+            style: TextStyle(
+              fontWeight: FontWeight.w600,
+              fontSize: 22,
+              letterSpacing: 0.27,
+              color: EcoAllureAppTheme.darkerText,
+            ),
+          ),
+          Flexible(
+            child: PopularBrandsListView(
+              callBack: () {
+                moveTo();
+              },
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  void moveTo() {
+    Navigator.push<dynamic>(
+      context,
+      MaterialPageRoute<dynamic>(
+        builder: (BuildContext context) => BrandInfoScreen(),
+      ),
+    );
+  }
+
+  Widget getButtonUI(CategoryType categoryTypeData, bool isSelected) {
+    String txt = '';
+    if (CategoryType.skincare == categoryTypeData) {
+      txt = 'Skincare';
+    } else if (CategoryType.makeup == categoryTypeData) {
+      txt = 'Makeup';
+    } else if (CategoryType.health == categoryTypeData) {
+      txt = 'Health';
+    }
+    return Expanded(
+      child: Container(
+        decoration: BoxDecoration(
+            color: isSelected
+                ? EcoAllureAppTheme.nearlyBlue
+                : EcoAllureAppTheme.nearlyWhite,
+            borderRadius: const BorderRadius.all(Radius.circular(24.0)),
+            border: Border.all(color: EcoAllureAppTheme.nearlyBlue)),
+        child: Material(
+          color: Colors.transparent,
+          child: InkWell(
+            splashColor: Colors.white24,
+            borderRadius: const BorderRadius.all(Radius.circular(24.0)),
+            onTap: () {
+              setState(() {
+                categoryType = categoryTypeData;
+              });
+            },
+            child: Padding(
+              padding: const EdgeInsets.only(
+                  top: 12, bottom: 12, left: 18, right: 18),
+              child: Center(
+                child: Text(
+                  txt,
+                  textAlign: TextAlign.left,
+                  style: TextStyle(
+                    fontWeight: FontWeight.w600,
+                    fontSize: 12,
+                    letterSpacing: 0.27,
+                    color: isSelected
+                        ? EcoAllureAppTheme.nearlyWhite
+                        : EcoAllureAppTheme.nearlyBlue,
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget getSearchBarUI() {
+    return Padding(
+      padding: const EdgeInsets.only(top: 8.0, left: 18),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Container(
+            width: MediaQuery.of(context).size.width * 0.75,
+            height: 64,
+            child: Padding(
+              padding: const EdgeInsets.only(top: 8, bottom: 8),
+              child: Container(
+                decoration: BoxDecoration(
+                  color: HexColor('#F8FAFB'),
+                  borderRadius: const BorderRadius.only(
+                    bottomRight: Radius.circular(13.0),
+                    bottomLeft: Radius.circular(13.0),
+                    topLeft: Radius.circular(13.0),
+                    topRight: Radius.circular(13.0),
+                  ),
+                ),
+                child: Row(
+                  children: <Widget>[
+                    Expanded(
+                      child: Container(
+                        padding: const EdgeInsets.only(left: 16, right: 16),
+                        child: TextFormField(
+                          style: TextStyle(
+                            fontFamily: 'WorkSans',
+                            fontWeight: FontWeight.bold,
+                            fontSize: 16,
+                            color: EcoAllureAppTheme.nearlyBlue,
+                          ),
+                          keyboardType: TextInputType.text,
+                          decoration: InputDecoration(
+                            labelText: 'Search for brand',
+                            border: InputBorder.none,
+                            helperStyle: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 16,
+                              color: HexColor('#B9BABC'),
+                            ),
+                            labelStyle: TextStyle(
+                              fontWeight: FontWeight.w600,
+                              fontSize: 16,
+                              letterSpacing: 0.2,
+                              color: HexColor('#B9BABC'),
+                            ),
+                          ),
+                          onEditingComplete: () {},
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      width: 60,
+                      height: 60,
+                      child: Icon(Icons.search, color: HexColor('#B9BABC')),
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ),
+          const Expanded(
+            child: SizedBox(),
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget getAppBarUI() {
+    return Padding(
+      padding: const EdgeInsets.only(top: 8.0, left: 18, right: 18),
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text(
+                  'EcoAllure',
+                  textAlign: TextAlign.left,
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 22,
+                    letterSpacing: 0.27,
+                    color: EcoAllureAppTheme.darkerText,
+                  ),
+                ),
+                Text(
+                  'better choices made easy',
+                  textAlign: TextAlign.left,
+                  style: TextStyle(
+                    fontWeight: FontWeight.w400,
+                    fontSize: 12,
+                    letterSpacing: 0.2,
+                    fontStyle: FontStyle.italic,
+                    color: EcoAllureAppTheme.grey,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Container(
+            width: 60,
+            height: 60,
+            child: Image.asset('assets/userImage.png'),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+enum CategoryType {
+  skincare,
+  makeup,
+  health,
+}
